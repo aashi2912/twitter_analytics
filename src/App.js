6898702612ef1470a86c9dbcecde105fdba07b59
Aashi <thakkaraashi3@gmail.com>
2023-03-21 19:58:42 -0400

    add theme with light and dark mode, add searchbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from "react";
+import SearchBar from "./components/SearchBar";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./components/theme";
 
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <SearchBar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
+
 export default App;
