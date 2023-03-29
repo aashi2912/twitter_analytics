7e96c8574441e5e99b4c91f1a4e17dfdd95975b5
Sahil Soni <43957583+iamsahilsoni@users.noreply.github.com>
2023-03-28 23:02:12 -0400

    add dashboard component for tweets and replies section (#8)

---
+++ src/enum/topic-title.enum.js
@@ -0,0 +1,19 @@
+const topicTitleEnum = {
+  "news&social_concern": "News and Social Concern",
+  "science&technology": "Science and Technology",
+  sports: "Sports",
+  "business&entrepreneurs": "Business and Entrepreneurs",
+  "diaries&daily_life": "Diaries and Daily Life",
+  "celebrity&pop_culture": "Celebrity and Pop Culture",
+  gaming: "Gaming",
+  "film_tv&video": "Film TV and Video",
+  "celebrity&pop_culture": "Celebrity and Pop Culture",
+  music: "Music",
+  "fitness&health": "Fitness and health",
+  "food&dining": "Food and Dining",
+  "learning&educational": "Learning and Educational",
+  family: "Family",
+  "fashion&style": "Fashion and Style",
+};
+
+export { topicTitleEnum };
