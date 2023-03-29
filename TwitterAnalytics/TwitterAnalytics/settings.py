73935bc73a9fced7aa31bfdedd7f2a43ab955e6d
iamsahilsoni <sonisahil12345@gmail.com>
2023-03-29 18:38:00 -0400

    get real data from db

--- TwitterAnalytics/TwitterAnalytics/settings.py
+++ TwitterAnalytics/TwitterAnalytics/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'TwitterAnalytics.urls'
@@ -122,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,28 @@ import React, { Component } from "react";
 import "./search.css";
 import search_logo from "../media/icon-search.png";
 
+const fetchUserAndOrHashtag = (input = "") => {
+  let user = null;
+  let hashtag = null;
+  let usrIdx = input.indexOf("@");
+  let tagIdx = input.indexOf("#");
+  if (usrIdx >= 0) {
+    let spaceIdx = input.indexOf(" ", usrIdx);
+    user = input.substring(
+      usrIdx + 1,
+      spaceIdx === -1 ? input.length + 1 : spaceIdx
+    );
+  }
+  if (tagIdx >= 0) {
+    let spaceIdx = input.indexOf(" ", tagIdx);
+    hashtag = input.substring(
+      tagIdx + 1,
+      spaceIdx === -1 ? input.length + 1 : spaceIdx
+    );
+  }
+  return { user, hashtag };
+};
+
 class SearchBar extends Component {
   constructor(props) {
     super(props);
@@ -16,7 +38,8 @@ class SearchBar extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    this.props.onSearch(this.state.searchTerm);
+    const { user, hashtag } = fetchUserAndOrHashtag(this.state.searchTerm);
+    this.props.onSearch({ user, hashtag });
   }
 
   render() {
